0b09cb8525690b609b8400f41dcb0c71be93c36d
Mike <migueldelatorre012@gmail.com>
2023-04-20 11:08:45 +0200

    Se genera en JS y html la interacion con los input manuales y la visualizacion de cada ventana(3)

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const article2 = document.querySelector('.article2');
 const article3 = document.querySelector('.article3');
 const confirmPrice = document.querySelector('#confirmBtnPrice');
 
-const discount1 = document.querySelector('.discount1');
-const discount2 = document.querySelector('.discount2');
-const discount3 = document.querySelector('.discount3');
+const discount1 = document.querySelector('#discount1');
+const discount2 = document.querySelector('#discount2');
+const discount3 = document.querySelector('#discount3');
 //const confirmDiscount = document.querySelector('#confirmBtnDiscount');
 const calcular = document.querySelector('#calcular');
+const resultDiscount = document.querySelector('#resultDiscount');
+const finalPrice = document.querySelector('#finalPrice');
+
+//const totalContainer = document.querySelector('#total-container');
 
 
 
@@ -27,58 +31,80 @@ discount3.addEventListener('click', agreeDiscount3);
 calcular.addEventListener('click', calcularTotal);
 
 
+var price;
+let discount;
+var totalPrice;
+var totalDiscount;
 
 
 
 function agreeprice1(){
-    var priceArticle1 = 100;
-    console.log(priceArticle1);
+    price = 100;
+    console.log(price);
 }
 
 function agreeprice2(){
-    var priceArticle2 = 600;
-    console.log(priceArticle2);
+    price = 600;
+    console.log(price);
 }
 
 function agreeprice3(){
-    var priceArticle3 = 1359;
-    console.log(priceArticle3);
+    price = 1359;
+    console.log(price);
 }
 
 function agreepriceManual(){
-    var priceManual = document.getElementById('price-input').value;
-    console.log(priceManual);
+    price = document.getElementById('price-input').value;
+    console.log(price);
+
+    if (price > 0){
+    showDiscountSection()
+}
 }
 
+function showDiscountSection(){
+    var discountSection = document.getElementById("discount-container");
+    discountSection.style.display = "block";
+}
 
 
 function agreeDiscount1(){
-    var discount1 = 10;
-    console.log(discount1);
-
-if (priceArticle1){
-    (discount1*priceArticle1)/100;
-}
-}
+     discount = 10;
+     console.log(discount);
+     }
 
 function agreeDiscount2(){
-    var discount2 = 15;
-    console.log(discount2);
+    discount = 15;
+    console.log(discount);
 }
 
 function agreeDiscount3(){
-    var discount3 = 30;
-    console.log(discount3);
+    discount = 30;
+    console.log(discount);
 }
-//function agreeDiscountManual(){
-    //var discountManual = document.getElementById('discount-input').value;
-    //console.log(discountManual);
-//}
+
+
+// function agreeDiscountManual(){
+//     var discountManual = document.getElementById('discount-input').value;
+//     console.log(discountManual);
+// }
+
+
+
 
 function calcularTotal(){
-    var discountManual = document.getElementById('discount-input').value;
-    console.log(discountManual);
-    console.log("Resultado final");
-}
 
+    discount = document.getElementById('discount-input').value;
+    totalDiscount=(discount*price)/100;
+    resultDiscount.innerHTML = "Total descuento: " + totalDiscount + " €";
+
+    totalPrice = (price-totalDiscount);
+    finalPrice.innerHTML = "El precio final con descuento es de: " + totalPrice + " €";
+
+    var totalContainer = document.getElementById("total-container");
+    if (discount > 0){
+       totalContainer.style.display = "block"; 
+    }
+       
+};
 
